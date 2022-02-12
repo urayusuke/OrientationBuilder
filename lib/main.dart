c1589970b70722e12f4c72b5e17c6e15bba757d5
浦佑輔 <urayuusuke@urayuusukenoMacBook-Air.local>
2022-02-12 22:30:06 +0900

    Flutterで画面の向きによってUIを更新する

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Orientation';
+
+    return const MaterialApp(
+      title: appTitle,
+      home: OrientationList(
+        title: appTitle,
+      ),
+    );
+  }
+}
+
+class OrientationList extends StatelessWidget {
+  final String title;
+
+  const OrientationList({Key? key, required this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.count(
+            crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+            children: List.generate(10, (index) {
+              return Center(
+                child: Text(
+                  '$index',
+                  style: const TextStyle(fontSize: 50),
+                ),
+              );
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
