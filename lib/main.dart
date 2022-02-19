9b303bd676299bebd57f2785d11777205d49d133
浦佑輔 <urayuusuke@urayuusukenoMacBook-Air.local>
2022-02-19 22:13:24 +0900

    テーマカラー設定

--- lib/main.dart
+++ lib/main.dart
@@ -9,39 +9,52 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Orientation';
+    const appName = 'アプリテーマ';
 
-    return const MaterialApp(
-      title: appTitle,
-      home: OrientationList(
-        title: appTitle,
+    return MaterialApp(
+      title: appName,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        fontFamily: 'Georgia',
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 32, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14, fontFamily: 'Hind'),
+        ),
+      ),
+      home: const MyHomePage(
+        title: appName,
       ),
     );
   }
 }
 
-class OrientationList extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
   final String title;
 
-  const OrientationList({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: OrientationBuilder(
-        builder: (context, orientation) {
-          return GridView.count(
-            crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
-            children: List.generate(10, (index) {
-              return Center(
-                child: Text(
-                  '$index',
-                  style: const TextStyle(fontSize: 50),
-                ),
-              );
-            }),
-          );
-        },
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Container(
+          color: Theme.of(context).colorScheme.secondary,
+          child: Text(
+            'テキストの背景',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ),
+      ),
+      floatingActionButton: Theme(
+        data: Theme.of(context).copyWith(splashColor: Colors.yellow),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
